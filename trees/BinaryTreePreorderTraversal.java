2f732df71010e08211181152261738416e991a8d
SushilGitt <sushilonecp@gmail.com>
2025-10-22 16:43:26 +0530

    Preorder iterative

--- trees/BinaryTreePreorderTraversal.java
+++ trees/BinaryTreePreorderTraversal.java
@@ -37,3 +37,35 @@ class Solution {
 
 // TC: O(n)
 // SC: O(n) -> Recursion call stack
+
+
+// Iterative solution
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList <Integer> arr = new ArrayList<>();
+        if(root == null) { 
+            return arr; 
+        }
+
+        ArrayDeque <TreeNode> s = new ArrayDeque<>();
+        s.push(root);
+
+        while(s.size() > 0) {
+            TreeNode node = s.pop();
+            arr.add(node.val);
+
+            if(node.right != null) {
+                s.push(node.right);
+            }
+
+            if(node.left != null) {
+                s.push(node.left);
+            }
+        }
+
+        return arr;
+    }
+}
+
+// TC: O(n)
+// SC: O(n)
